2ce1d093f1ed8b0ed443ed24785b8148af0ae5c8
Howard Chen <hchen7913@gmail.com>
2019-04-13 21:09:32 -0700

    Test load, store, and add operations

--- computer/operator_test.go
+++ computer/operator_test.go
@@ -3,7 +3,10 @@
 
 package computer
 
-import "testing";
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+);
 
 type MemoryMock struct {
 	val uint32
@@ -17,31 +20,36 @@ func (m *MemoryMock) Set(address uint16, val uint32) {
 	m.val = val
 } 
 
-func TestAdd(t *testing.T) {
+func TestLoadStore(t *testing.T) {
 	var operator Operator =  Operator {};
-	if operator.registers[0] != 0 {
-		// Sanity check on initialization.
-		t.Errorf("Expected 0, got %d", operator.registers[0] );
-	}
+	var assert = assert.New(t);
+
+	var zero uint32 = 0;
+	assert.Equal(operator.registers[0], zero);
 
 	var start_value uint32 = 10
 	memory := MemoryMock { val: start_value }
-	if memory.val != start_value {
-		t.Errorf("Expected %d, got %d", start_value, memory.val);
-	}
+	assert.Equal(memory.val, start_value);
 
 	operator.load(0, 5, &memory);
-	if operator.registers[0] != start_value {
-		t.Errorf("Expected %d, got %d", start_value, operator.registers[0]);
-	}
-
+	assert.Equal(operator.registers[0], start_value);
+	
 	operator.load(15, 5, &memory);
-	if operator.registers[15] != start_value {
-		t.Errorf("Expected %d, got %d", start_value, operator.registers[15])
-	}
+	assert.Equal(operator.registers[15], start_value);
 
 	operator.store(1, 5, &memory);
-	if memory.val != 0 {
-		t.Errorf("Expected %d, got %d", 0, memory.val);
-	}
+	assert.Equal(memory.val, zero);
+}	
+
+func TestAdd(t *testing.T) {
+	var operator Operator =  Operator {};
+	var assert = assert.New(t);
+
+	var start_value uint32 = 10
+	memory := MemoryMock { val: start_value }
+	operator.load(0, 5, &memory);
+	operator.load(15, 5, &memory);
+
+	operator.add(5, 0, 15);
+	assert.Equal(operator.registers[5], start_value * 2);
 }
