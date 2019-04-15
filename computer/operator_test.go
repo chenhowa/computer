eb550992a56ed569d244f56ef8b4bd65b223b775
Howard Chen <hchen7913@gmail.com>
2019-04-14 18:53:55 -0700

    Add test basics

--- computer/operator_test.go
+++ computer/operator_test.go
@@ -1,12 +1,10 @@
-
-
-
 package computer
 
 import (
 	"testing"
+
 	"github.com/stretchr/testify/assert"
-);
+)
 
 type MemoryMock struct {
 	val uint32
@@ -18,38 +16,42 @@ func (m *MemoryMock) Get(address uint16) uint32 {
 
 func (m *MemoryMock) Set(address uint16, val uint32) {
 	m.val = val
-} 
+}
 
 func TestLoadStore(t *testing.T) {
-	var operator Operator =  Operator {};
-	var assert = assert.New(t);
+	var operator Operator = Operator{}
+	var assert = assert.New(t)
 
-	var zero uint32 = 0;
-	assert.Equal(operator.registers[0], zero);
+	var zero uint32 = 0
+	assert.Equal(operator.registers[0], zero)
 
 	var start_value uint32 = 10
-	memory := MemoryMock { val: start_value }
-	assert.Equal(memory.val, start_value);
+	memory := MemoryMock{val: start_value}
+	assert.Equal(memory.val, start_value)
 
-	operator.load(0, 5, &memory);
-	assert.Equal(operator.registers[0], start_value);
-	
-	operator.load(15, 5, &memory);
-	assert.Equal(operator.registers[15], start_value);
+	operator.load(0, 5, &memory)
+	assert.Equal(operator.registers[0], start_value)
 
-	operator.store(1, 5, &memory);
-	assert.Equal(memory.val, zero);
-}	
+	operator.load(15, 5, &memory)
+	assert.Equal(operator.registers[15], start_value)
+
+	operator.store(1, 5, &memory)
+	assert.Equal(memory.val, zero)
+}
 
 func TestAdd(t *testing.T) {
-	var operator Operator =  Operator {};
-	var assert = assert.New(t);
+	var operator Operator = Operator{}
+	var assert = assert.New(t)
 
 	var start_value uint32 = 10
-	memory := MemoryMock { val: start_value }
-	operator.load(0, 5, &memory);
-	operator.load(15, 5, &memory);
+	memory := MemoryMock{val: start_value}
+	operator.load(0, 5, &memory)
+	operator.load(15, 5, &memory)
 
-	operator.add(5, 0, 15);
-	assert.Equal(operator.registers[5], start_value * 2);
+	operator.add(5, 0, 15)
+	assert.Equal(operator.registers[5], start_value*2)
+}
+
+func TestSub(t *testing.T) {
+
 }
