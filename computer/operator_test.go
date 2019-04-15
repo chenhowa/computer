0fde44f207ed0fec5c6731d2c39ab0f9a71a31fd
Howard Chen <hchen7913@gmail.com>
2019-04-14 19:13:39 -0700

    Convert to suite-based test tools

--- computer/operator_test.go
+++ computer/operator_test.go
@@ -4,7 +4,14 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
+
+type OperatorSuite struct {
+	suite.Suite
+	operator Operator
+	memory   MemoryMock
+}
 
 type MemoryMock struct {
 	val uint32
@@ -18,40 +25,47 @@ func (m *MemoryMock) Set(address uint16, val uint32) {
 	m.val = val
 }
 
-func TestLoadStore(t *testing.T) {
-	var operator Operator = Operator{}
-	var assert = assert.New(t)
+func (suite *OperatorSuite) SetupTest() {
+	suite.operator = Operator{}
+	suite.memory = MemoryMock{}
+}
+
+func TestRun(t *testing.T) {
+	suite.Run(t, new(OperatorSuite))
+}
+
+func (suite *OperatorSuite) TestLoadStore() {
+	var assert = assert.New(suite.T())
 
 	var zero uint32 = 0
-	assert.Equal(operator.registers[0], zero)
+	assert.Equal(suite.operator.registers[0], zero)
 
 	var start_value uint32 = 10
-	memory := MemoryMock{val: start_value}
-	assert.Equal(memory.val, start_value)
+	suite.memory.val = start_value
+	assert.Equal(suite.memory.val, start_value)
 
-	operator.load(0, 5, &memory)
-	assert.Equal(operator.registers[0], start_value)
+	suite.operator.load(0, 5, &suite.memory)
+	assert.Equal(suite.operator.registers[0], start_value)
 
-	operator.load(15, 5, &memory)
-	assert.Equal(operator.registers[15], start_value)
+	suite.operator.load(15, 5, &suite.memory)
+	assert.Equal(suite.operator.registers[15], start_value)
 
-	operator.store(1, 5, &memory)
-	assert.Equal(memory.val, zero)
+	suite.operator.store(1, 5, &suite.memory)
+	assert.Equal(suite.memory.val, zero)
 }
 
-func TestAdd(t *testing.T) {
-	var operator Operator = Operator{}
-	var assert = assert.New(t)
+func (suite *OperatorSuite) TestAdd() {
+	var assert = assert.New(suite.T())
 
 	var start_value uint32 = 10
-	memory := MemoryMock{val: start_value}
-	operator.load(0, 5, &memory)
-	operator.load(15, 5, &memory)
+	suite.memory.val = start_value
+	suite.operator.load(0, 5, &suite.memory)
+	suite.operator.load(15, 5, &suite.memory)
 
-	operator.add(5, 0, 15)
-	assert.Equal(operator.registers[5], start_value*2)
+	suite.operator.add(5, 0, 15)
+	assert.Equal(suite.operator.registers[5], start_value*2)
 }
 
-func TestSub(t *testing.T) {
+func (suite *OperatorSuite) TestSub() {
 
 }
