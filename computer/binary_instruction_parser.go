2a38b098bfba28fad67100ab5ea1d99eac95f4c9
Howard Chen <hchen7913@gmail.com>
2019-04-27 16:08:51 -0700

    Implemented parsing module for 32 bit instructions

--- computer/binary_instruction_parser.go
+++ computer/binary_instruction_parser.go
@@ -5,9 +5,13 @@ import "fmt"
 type riscVBinaryInstructionParser struct {
 }
 
+/*
+	Will need to write a connector between this parser and the assembly executor
+	that will use it.
+*/
+
 func (parser *riscVBinaryInstructionParser) parse(instruction uint32) riscVBinaryParseResult {
-	opcode := 127 & instruction
-	var instructionType instructionType
+	opcode := ((1 << 7) - 1) & instruction
 	var result riscVBinaryParseResult
 
 	if opcode < 3 { // immediate arithmetic
@@ -29,53 +33,119 @@ func (parser *riscVBinaryInstructionParser) parse(instruction uint32) riscVBinar
 	} else {
 		panic(fmt.Sprintf("unrecognized opcode %d", opcode))
 	}
+	result.errorIfInvalid()
 
-	return riscVBinaryParseResult{}
+	return result
+}
+
+/*getBitsInInclusiveRange takes a number and extracts the value of a certain set of bits,
+  from bit `start` to bit `end`. `start` is assumed to be less than `end`.
+  The 0th bit is assumed to be the Least-Significant-Bit (LSB) of `number`
+
+  Examples: when `number` = 0b000011100, `start` = 1, `end` = 4, the return value will be 0b000001110 (as uint)
+			when `number` = 0b000011100, `start` = 0, `end` = 2, the return value will be 0b000000100 (as uint)
+*/
+func getBitsInInclusiveRange(number uint, start uint, end uint) uint {
+	if start > end {
+		panic(fmt.Sprintf("getBitsInInclusiveRange: start > end, start was %d, end was %d", start, end))
+	}
+
+	shifted := number >> start
+	var masklength = end - start
+	var mask uint = (1 << ((masklength) + 1)) - 1
+	return shifted & mask
 }
 
 func parseAsI(instruction uint32) riscVBinaryParseResult {
+	var uintInstruction = uint(instruction)
+	result := riscVBinaryParseResult{instructionType: I}
+	result.fiveBitDestination = uint8(getBitsInInclusiveRange(uintInstruction, 7, 11))
+	result.funct3 = uint8(getBitsInInclusiveRange(uintInstruction, 12, 14))
+	result.fiveBitRegister1 = uint8(getBitsInInclusiveRange(uintInstruction, 15, 19))
+	result.twelveBitImmediate = uint16(getBitsInInclusiveRange(uintInstruction, 20, 31))
 
+	return result
 }
 
 func parseAsR(instruction uint32) riscVBinaryParseResult {
+	result := riscVBinaryParseResult{instructionType: R}
+	var uintInstruction = uint(instruction)
+	result.fiveBitDestination = uint8(getBitsInInclusiveRange(uintInstruction, 7, 11))
+	result.funct3 = uint8(getBitsInInclusiveRange(uintInstruction, 12, 14))
+	result.fiveBitRegister1 = uint8(getBitsInInclusiveRange(uintInstruction, 15, 19))
+	result.fiveBitRegister2 = uint8(getBitsInInclusiveRange(uintInstruction, 20, 24))
+	result.funct7 = uint8(getBitsInInclusiveRange(uintInstruction, 25, 31))
 
+	return result
 }
 
 func parseAsJ(instruction uint32) riscVBinaryParseResult {
+	result := riscVBinaryParseResult{instructionType: J}
+	var uintInstruction = uint(instruction)
+	result.fiveBitDestination = uint8(getBitsInInclusiveRange(uintInstruction, 7, 11))
 
+	bits1To10 := getBitsInInclusiveRange(uintInstruction, 21, 30)
+	bit11 := getBitsInInclusiveRange(uintInstruction, 20, 20)
+	bits12To19 := getBitsInInclusiveRange(uintInstruction, 12, 19)
+	bit20 := getBitsInInclusiveRange(uintInstruction, 31, 31)
+
+	result.twentyBitImmediate = uint32(bits1To10 | (bit11 << 10) | (bits12To19 << 11) | (bit20 << 19))
+	return result
 }
 
 func parseAsS(instruction uint32) riscVBinaryParseResult {
+	result := riscVBinaryParseResult{instructionType: S}
+	var uintInstruction = uint(instruction)
+	result.funct3 = uint8(getBitsInInclusiveRange(uintInstruction, 12, 14))
+	result.fiveBitRegister1 = uint8(getBitsInInclusiveRange(uintInstruction, 15, 19))
+	result.fiveBitRegister2 = uint8(getBitsInInclusiveRange(uintInstruction, 20, 24))
 
+	lowerFiveBits := getBitsInInclusiveRange(uintInstruction, 0, 4)
+	upper7Bits := getBitsInInclusiveRange(uintInstruction, 25, 31)
+	result.twelveBitImmediate = uint16(lowerFiveBits | (upper7Bits << 5))
+
+	return result
 }
 
 func parseAsB(instruction uint32) riscVBinaryParseResult {
+	result := riscVBinaryParseResult{instructionType: B}
+	var uintInstruction = uint(instruction)
+	result.funct3 = uint8(getBitsInInclusiveRange(uintInstruction, 12, 14))
+	result.fiveBitRegister1 = uint8(getBitsInInclusiveRange(uintInstruction, 15, 19))
+	result.fiveBitRegister2 = uint8(getBitsInInclusiveRange(uintInstruction, 20, 24))
 
+	bits1To4 := getBitsInInclusiveRange(uintInstruction, 8, 11)
+	bits5To10 := getBitsInInclusiveRange(uintInstruction, 25, 30)
+	bit11 := getBitsInInclusiveRange(uintInstruction, 11, 11)
+	bit12 := getBitsInInclusiveRange(uintInstruction, 31, 31)
+	result.twelveBitImmediate = uint16(bits1To4 | (bits5To10 << 4) | (bit11 << 10) | (bit12 << 11))
+
+	return result
 }
 
 type instructionType uint
 
+/*These constants represent the various available instruction types available in the RiscV instruction set (32 bits)
+ */
 const (
-	// the below instruction types are the base instruction types
-	r instructionType = iota + 1
-	i
-	s
-	u
-	b
+	R instructionType = iota + 1 // the below instruction types are the base instruction types
+	I
+	S
+	U
+	B
 
-	// the below constants are extension instruction types. see the online specification for more.
-	m
-	a
-	f
-	d
-	q
-	l
-	c
-	j
-	t
-	p
-	v
-	n
+	M // the below constants are extension instruction types. see the online specification for more.
+	A
+	F
+	D
+	Q
+	L
+	C
+	J
+	T
+	P
+	V
+	N
 )
 
 type riscVBinaryParseResult struct {
@@ -113,8 +183,3 @@ func (result riscVBinaryParseResult) errorIfInvalid() {
 func xIsGreaterThanYBits(x uint, y uint) bool {
 	return x >= (1 >> y)
 }
-
-/*
-	Will need to write a connector between this parser and the assembly executor
-	that will use it.
-*/
